2f0ad1cf3c3e10343dbbfca8ef6f2c389a9ec7c0
acastillom <81824205+acastillom24@users.noreply.github.com>
2021-05-27 17:36:50 -0500

    Create functionPRIMER_DIA_MES.js

---
+++ functionPRIMER_DIA_MES.js
@@ -0,0 +1,7 @@
+// Calcula el primer día de cada mes
+function PRIMER_DIA_MES(rango) {
+  return Array.isArray(rango)
+    ? rango.map(el1 => el1.map(el2 => ((
+      el2.getMonth() + 1) + "/01/" + el2.getFullYear())))
+    : new Date((rango.getMonth() + 1) + "/01/" + rango.getFullYear())
+}
